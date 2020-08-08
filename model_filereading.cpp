2285e5e432fce42d0ebff2c26076f2ac70e9cc78
rlavendomme <65511614+rlavendomme@users.noreply.github.com>
2020-08-09 01:03:08 +0200

    Atomic number read from radii file and improve xyz reading
    - Added a function to fix wrongly written element symbols (e.g. PD2+ instead of Pd, fe instead of Fe).
    - Changed the function that stored atomic radii to also store atomic number (need to use the new definition file that include atomic numbers).
    - Added a clear function to clear the unordered maps of radii and atomic number when the calculation is rerun.
    - Modified the xyz line reading algorithm to avoid issues with unusual xyz formats. Some of them don't have the number of atoms at the beginning or a comment line and can have several lines unrelated to atom coordinates. Thus I added a check for a valid atom coordinates line format and only these lines are read to generate the atom map.
    - I left the line_counter variable counting the number of lines in the structure file but it is not used anymore. Could be removed if we have no future plans for it.

--- model_filereading.cpp
+++ model_filereading.cpp
@@ -10,19 +10,40 @@
 #include <iterator>
 
 // split line into substrings when separated by whitespaces
-static inline std::vector<std::string> splitLine(std::string& line){ 
+static inline std::vector<std::string> splitLine(std::string& line){
   std::istringstream iss(line);
   std::vector<std::string> substrings((std::istream_iterator<std::string>(iss)), std::istream_iterator<std::string>());
   return substrings;
 }
 
-// reads radii from a file specified by the filepath and 
+// Reads a string and converts it to valid atom symbol with first char upper case then lower case chars
+std::string strToValidSymbol(const std::string& str_inp){
+  std::string validSymbol = "";
+  for (int i = 0; str_inp[i]!=0; i++) {
+    if (isalpha(str_inp[i])) {
+      validSymbol = validSymbol + str_inp[i];
+      if (i==0 && islower(validSymbol[i])) {
+        validSymbol[i] = toupper(validSymbol[i]);
+      }
+      else if (i>0 && isupper(validSymbol[i])) {
+        validSymbol[i] = tolower(validSymbol[i]);
+      }
+    }
+    else {
+      return validSymbol;
+    }
+  }
+}
+
+// reads radii from a file specified by the filepath and
 // stores them in an unordered map that is an attribute of
 // the Model object
-void Model::readRadiiFromFile(std::string& filepath){
- 
+void Model::readRadiiAndAtomNumFromFile(std::string& filepath){
+  // clear unordered_maps to avoid keeping data from previous runs
+  radii.clear();
+  elem_Z.clear();
   //TODO add case handling for when file has already been read
-  //TODO if map empty: ask user via dialog boy if they want to
+  //TODO if map empty: ask user via dialog box if they want to
   //TODO reimport the file
   //TODO make a function "consultUser(string)"
   //if(radii.empty()){std::cout << "empty" << std::endl;}
@@ -32,8 +53,9 @@ void Model::readRadiiFromFile(std::string& filepath){
 
   while(getline(inp_file,line)){
     std::vector<std::string> substrings = splitLine(line);
-    if(substrings.size() == 2){
-      radii[substrings[0]] = std::stod(substrings[1]);
+    if(substrings.size() == 3){
+      radii[substrings[1]] = std::stod(substrings[2]);
+      elem_Z[substrings[1]] = std::stoi(substrings[0]);
     }
   }
   return;
@@ -55,7 +77,7 @@ inline double Model::findRadiusOfAtom(const Atom& at){
 
 void Model::readAtomsFromFile(std::string& filepath){
 
-  int n_atom;
+  int n_atom = 0;
   std::vector<Atom> list_of_atoms;
 
 //if (inp_file.is_open()){  //TODO consider adding an exception, for when file in not valid
@@ -64,31 +86,27 @@ void Model::readAtomsFromFile(std::string& filepath){
   std::string line;
   std::ifstream inp_file(filepath);
 
-  int line_counter = 0;
+  int line_counter = 0; // Might not be useful anymore
   // iterate through lines
-  while(getline(inp_file,line)){
-  
-    if(line_counter==0){ // first line contains number of atoms
-      n_atom = std::stoi(line);
-    }
-    
-
-    else if(line_counter == 1){ // comment line (ignore)
-    }
-    
-    else { // subsequent lines contain atom positional data
-      std::vector<std::string> substrings = splitLine(line);
-      for(std::string str : substrings){
-      }
-      // create new atom and add to storage vector (assumes file format: Element_Symbol x y z)
-      Atom at = Atom(std::stod(substrings[1]), 
-                     std::stod(substrings[2]), 
-                     std::stod(substrings[3]), 
-                     substrings[0], 
-                     radii[substrings[0]]);
+  while(getline(inp_file,line)){ // ws to pass empty lines and blank space before atoms if any
+    // divide line into "words"
+    std::vector<std::string> substrings = splitLine(line);
+    // create new atom and add to storage vector if line format corresponds to Element_Symbol x y z
+    if (substrings.size() == 4
+        && isalpha(substrings[0][0])
+        && (isdigit(substrings[1][0]) || ispunct(substrings[1][0])) // ispunct is necessary in case of negative value starting with '-'
+        && (isdigit(substrings[2][0]) || ispunct(substrings[2][0]))
+        && (isdigit(substrings[3][0]) || ispunct(substrings[3][0]))) {
+      Atom at = Atom(std::stod(substrings[1]),
+                     std::stod(substrings[2]),
+                     std::stod(substrings[3]),
+                     strToValidSymbol(substrings[0]),
+                     radii[strToValidSymbol(substrings[0])],
+                     elem_Z[strToValidSymbol(substrings[0])]);
       list_of_atoms.push_back(at);
+      n_atom++;
     }
-    line_counter++;
+    line_counter++; // Might not be useful anymore
   }
   // file has been read
   inp_file.close();
@@ -99,6 +117,6 @@ void Model::readAtomsFromFile(std::string& filepath){
 }
 
 /*void Model::lookUpRadii(std::string& filepath){
-  
+
   return;
 }*/
