5da66a0823384394546ad62f8d8620fc45ecafd9
Jasmin B. Maglic <jmaglic@outlook.de>
2020-08-10 12:06:58 +0200

    Rework readAtomsFromFile method in Model class
    As part of this method, lines from the input file need to be analysed to
    determine whether they hold valid atom information, i.e., are of format
    <Element Symbol; x-coordinate; y-coordinate; z-coordinate>. Previous to
    this change, this assessment was only based on the first character of
    each of these properties. For instance, if the x-coodinate was contained
    in a substring "3.5", only the first character '3' would be assessed and
    the whole string would be deemed numerical. Now, the whole substring is
    taken into account.

--- model_filereading.cpp
+++ model_filereading.cpp
@@ -9,6 +9,17 @@
 #include <exception>
 #include <iterator>
 
+///////////////////////////////
+// AUX FUNCTION DECLARATIONS //
+///////////////////////////////
+
+bool isAtomLine(const std::vector<std::string>& substrings);
+std::string strToValidSymbol(std::string str);
+
+////////////////////////
+// METHOD DEFINITIONS //
+////////////////////////
+
 // split line into substrings when separated by whitespaces
 static inline std::vector<std::string> splitLine(std::string& line){
   std::istringstream iss(line);
@@ -16,21 +27,6 @@ static inline std::vector<std::string> splitLine(std::string& line){
   return substrings;
 }
 
-// reads a string and converts it to valid atom symbol: first character uppercase followed by lowercase characters
-std::string strToValidSymbol(std::string str){
-  // iterate over all characters in string
-  for (int i = 0; i<str.size(); i++) {
-    // only for first character in sequence, convert to uppercase
-    if (i==0) {
-      str[i] = toupper(str[i]);
-    }
-    // for all other characters, convert to lowercase
-    else {
-      str[i] = tolower(str[i]);
-    }
-  }
-  return str;
-}
 
 // reads radii from a file specified by the filepath and
 // stores them in an unordered map that is an attribute of
@@ -74,46 +70,67 @@ inline double Model::findRadiusOfAtom(const Atom& at){
 
 void Model::readAtomsFromFile(std::string& filepath){
 
-  int n_atom = 0;
   std::vector<Atom> list_of_atoms;
 
 //if (inp_file.is_open()){  //TODO consider adding an exception, for when file in not valid
-
-  // we iterate through the lines in the input file
   std::string line;
   std::ifstream inp_file(filepath);
 
-  int line_counter = 0; // Might not be useful anymore
   // iterate through lines
   while(getline(inp_file,line)){ // ws to pass empty lines and blank space before atoms if any
     // divide line into "words"
     std::vector<std::string> substrings = splitLine(line);
-    // create new atom and add to storage vector if line format corresponds to Element_Symbol x y z
-    if (substrings.size() == 4
-        && isalpha(substrings[0][0])
-        && (isdigit(substrings[1][0]) || ispunct(substrings[1][0])) // ispunct is necessary in case of negative value starting with '-'
-        && (isdigit(substrings[2][0]) || ispunct(substrings[2][0]))
-        && (isdigit(substrings[3][0]) || ispunct(substrings[3][0]))) {
-      Atom at = Atom(std::stod(substrings[1]),
+    if (substrings.size() == 4) {
+      // create new atom and add to storage vector if line format corresponds to Element_Symbol x y z
+      if (isAtomLine(substrings)) {
+
+        Atom at = Atom(std::stod(substrings[1]),
                      std::stod(substrings[2]),
                      std::stod(substrings[3]),
                      strToValidSymbol(substrings[0]),
                      radii[strToValidSymbol(substrings[0])],
                      elem_Z[strToValidSymbol(substrings[0])]);
-      list_of_atoms.push_back(at);
-      n_atom++;
+        list_of_atoms.push_back(at);
+      
+      }
     }
-    line_counter++; // Might not be useful anymore
   }
   // file has been read
   inp_file.close();
-  // check if number of atoms matches size of the vector containing the atoms
-  assert(n_atom == list_of_atoms.size());
+  
   this->atoms = list_of_atoms;
   return;
 }
 
-/*void Model::lookUpRadii(std::string& filepath){
+///////////////////
+// AUX FUNCTIONS //
+///////////////////
 
-  return;
-}*/
+bool isAtomLine(const std::vector<std::string>& substrings) {
+  for (char i=1; i<4; i++){
+    // using a try-block feels hacky, but the alternative using strtod does as well
+    try {
+      std::stod(substrings[i]);
+    }
+    catch (const std::invalid_argument& ia) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// reads a string and converts it to valid atom symbol: first character uppercase followed by lowercase characters
+std::string strToValidSymbol(std::string str){
+  // iterate over all characters in string
+  for (int i = 0; i<str.size(); i++) {
+    // only for first character in sequence, convert to uppercase
+    if (i==0) {
+      str[i] = toupper(str[i]);
+    }
+    // for all other characters, convert to lowercase
+    else {
+      str[i] = tolower(str[i]);
+    }
+  }
+  return str;
+}
