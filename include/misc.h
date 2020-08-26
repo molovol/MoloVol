e022b0e8a7c69dd78f65310c5a99bb081335e5f2
Jasmin B. Maglic <jmaglic@outlook.de>
2020-08-26 23:35:58 +0200

    Split misc.h into header and cpp files

--- include/misc.h
+++ include/misc.h
@@ -2,6 +2,7 @@
 
 #define MISC_H
 
+#include <string>
 #include <array>
 #include <cmath>
 
@@ -13,16 +14,6 @@ inline double distance(const std::array<double,3> &start, const std::array<doubl
   return end[dim]-start[dim];
 }
 
-inline std::string fileExtension(const std::string& path){
-  // will cause an issue, if there is a dot in the middle of the file AND no file extension
-  std::string after_dot = "";
-  int dot_pos = path.find_last_of(".");
-  if (dot_pos != std::string::npos){
-    return path.substr(dot_pos+1);
-  }
-  else{
-    return "invalid";
-  }
-}
+std::string fileExtension(const std::string& path);
 
 #endif
