dd7e3f5236d607b6ef3fd5ddf141df6498791850
Jasmin B. Maglic <jmaglic@outlook.de>
2020-12-04 12:08:21 +0100

    Avoid passing down universal args for voxel type
    Static variables have been added to the Voxel class in order to avoid
    passing these as arguments. These variables contain resources needed by
    all voxels for type assignment. This way, all voxels have access to
    these resources.
    
    Resolves: #54

--- include/voxel.h
+++ include/voxel.h
@@ -3,6 +3,7 @@
 #define VOXEL_H
 
 #include "vector.h"
+#include "atomtree.h"
 #include <vector>
 #include <array>
 
@@ -17,20 +18,15 @@ class Voxel{
     void setType(char);
     char getType();
     
-    //static void storeUniversalVariables();
+    static void storeUniversal(AtomTree, double, double);
 
     char determineType(
         std::array<double,3> pos,
-        const double& grid_size,
-        const double& r_probe,
-        const double max_depth,
-        const AtomTree& atomtree);
+        const double max_depth);
     
     void determineTypeSingleAtom(
         const Atom& atom, 
         std::array<double,3> pos, // voxel centre
-        const double& grid_size,
-        const double& r_probe,
         const double max_depth,
         bool&);
    
@@ -40,19 +36,18 @@ class Voxel{
         const double& at_rad, 
         const double& vxl_rad, 
         const std::array<double,3> vxl_pos,
-        const double& grid_size, 
-        const double& r_probe, 
         const double& max_depth,
         bool&);
     size_t tallyVoxelsOfType(const char volume_type, const int max_depth);
 
     void splitVoxel(
         const std::array<double,3>& vxl_pos, 
-        const double& grid_size, 
-        const double& r_probe, 
-        const double& max_depth, 
-        const AtomTree& atom_tree);
+        const double& max_depth); 
   private:
+
+    static inline AtomTree _atomtree;
+    static inline double _grid_size;
+    static inline double _r_probe1;
 
     bool isAtom(const Atom& atom, const double& dist_vxl_at, const double& radius_of_influence); // inline not faster
     bool isAtAtomEdge(const Atom& atom, const double& dist_vxl_at, const double& radius_of_influence); // inline not faster
