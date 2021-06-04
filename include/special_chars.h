2966b881d6f00a7718ffe97ad75df8a28941f092
Jasmin B. Maglic <jmaglic@outlook.de>
2021-06-04 18:07:24 +0200

    Remove unused code

--- include/special_chars.h
+++ include/special_chars.h
@@ -10,11 +10,8 @@ class Symbol{
     static std::wstring angstrom();
     static std::wstring squared();
     static std::wstring cubed();
-    static std::wstring numSubscript(int num);
-    static std::wstring numSubscript(std::string num);
     static wchar_t digitSubscript(char digit);
     static std::wstring generateChemicalFormulaUnicode(std::string);
-
 };
 
 #endif
