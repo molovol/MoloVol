368fd2dfeefe3c3d6b6d7c518b08dbca987fdd9a
Jasmin B. Maglic <jmaglic@outlook.de>
2021-08-01 22:09:54 +0200

    Simplify getTotalVolPerType function

--- src/cavity.cpp
+++ src/cavity.cpp
@@ -43,29 +43,20 @@ void inverseSort(std::vector<Cavity>& cavities){
 
 // only valid for isolated structures, not for unit cell analysis
 std::array<double, 3> getTotalVolPerType(const std::vector<Cavity>& cavities, const bool probe_mode){
-  std::array<double, 3> volume_per_type;
+  std::array<double, 3> volume_per_type = {0,0,0};
   if(probe_mode){
-    for(unsigned int i = 0; i < cavities.size(); i++){
-      unsigned int type;
-      if(cavities[i].getNumberEntrances() == 0){
-        type = 0;
-      }
-      else if(cavities[i].getNumberEntrances() == 1){
-        type = 1;
-      }
-      else{
-        type = 2;
-      }
-      volume_per_type[type] += cavities[i].getVolume();
+    for(const auto& cav : cavities){
+      int type = cav.getNumberEntrances();
+      if (type > 2) {type = 2;}
+      volume_per_type[type] += cav.getVolume();
     }
   }
   else{
   // in single probe mode, the first cavity with id 1 is the outside space plus pockets and tunnels
   // thus there is no physical meaning
-    for(unsigned int i = 0; i < cavities.size(); i++){
-      if(cavities[i].id > 1){
-        volume_per_type[0] += cavities[i].getVolume();
-      }
+    for(const auto& cav : cavities){
+      if(cav.id == 1){continue;}
+      volume_per_type[0] += cav.getVolume();
     }
   }
   return volume_per_type;
