81b60e7cc52bebea347997d6b2fd88b3c35ec379
Benedikt S. Vogler <developer@benediktsvogler.com>
2021-11-14 23:30:38 +0100

    add form for input
    Using the form for input is not working yet, however when you accept HTML the output is working.

--- webserver/app.py
+++ webserver/app.py
@@ -1,14 +1,20 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import subprocess
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def hello_world():
-    args: list[str] = request.args.get('cli-arguments', '').split(" ")
-    try:
-        returnvalues = subprocess.check_output(["./launch_headless.sh"] + args, stderr=subprocess.STDOUT)
-    except Exception as e:
-        return "Exception:" + str(e)
-    return jsonify({"output": returnvalues.decode("utf-8")})
+@app.route('/', methods=['GET', 'POST'])
+def io():
+    returnvalues = None
+    if request.method == 'POST':
+        args: list[str] = request.args.get('cli-arguments', '').split(" ")
+        try:
+            returnvalues = subprocess.check_output(["./launch_headless.sh"] + args, stderr=subprocess.STDOUT).decode(
+                "utf-8")
+        except Exception as e:
+            returnvalues = "Exception: " + str(e)
+    if request.accept_mimetypes['text/html']:
+        return render_template('form.html', returnvalues=returnvalues)
+    elif request.accept_mimetypes['application/json']:
+        return jsonify({"output": returnvalues})
