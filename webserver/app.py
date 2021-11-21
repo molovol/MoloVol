214c41e81c8087acb322ac943e89d390b4b6d28e
Benedikt S. Vogler <developer@benediktsvogler.com>
2021-11-21 15:50:59 +0100

    allow input from form

--- webserver/app.py
+++ webserver/app.py
@@ -6,15 +6,27 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def io():
-    returnvalues = None
+    out = None
+    inputdict = {}
     if request.method == 'POST':
-        args: list[str] = request.args.get('cli-arguments', '').split(" ")
+        # when arguments ignore form data
+        # split cli string so that it can be passed to subprocess
+        args: list[str] = []
+        if len(request.form) > 0:
+            for key, value in request.form.items():
+                args.append(f"--{key}")
+                if value != "":
+                    args.append(str(value))
+            inputdict = request.form
+        else:
+            args = request.args.get('cli-arguments', '').split(" ")
+            inputdict = request.args
         try:
-            returnvalues = subprocess.check_output(["./launch_headless.sh"] + args, stderr=subprocess.STDOUT).decode(
+            out = subprocess.check_output(["./launch_headless.sh"] + args, stderr=subprocess.STDOUT).decode(
                 "utf-8")
         except Exception as e:
-            returnvalues = "Exception: " + str(e)
+            out = "Exception: " + str(e)
     if request.accept_mimetypes['text/html']:
-        return render_template('form.html', returnvalues=returnvalues)
+        return render_template('form.html', inputdict=inputdict, returnvalues=out)
     elif request.accept_mimetypes['application/json']:
-        return jsonify({"output": returnvalues})
+        return jsonify({"output": out})
