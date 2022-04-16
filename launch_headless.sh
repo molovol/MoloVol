f79ba603e947092c61a4a5ede551290f123fbb03
Benedikt S. Vogler <developer@benediktsvogler.com>
2022-04-16 22:11:52 +0200

    split webserver and dock section

--- launch_headless.sh
+++ launch_headless.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# This script launches the headless version of the application. It creates a fake display so that wxwidgets is happy.
 Xvfb :1 -screen 0 1024x768x16 &> xvfb.log  &
 /bin/MoloVol $@
