b618598943a8a9000168b8935bb14551fa4ec03f
Benedikt S. Vogler <developer@benediktsvogler.com>
2022-04-16 22:11:38 +0200

    improve form handling

--- webserver/app.py
+++ webserver/app.py
@@ -1,7 +1,37 @@
+import os
+from typing import Optional
+
 from flask import Flask, request, jsonify, render_template
 import subprocess
 
+from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
+
+ALLOWED_EXTENSIONS = {'xyz', 'pdb', 'txt', 'cif'}
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def manage_uploaded_file(request, filetype="structure"):
+    global out
+    path: Optional[str] = None
+    if filetype in request.files:
+        # try using last value
+        structure_file = request.files['structure']
+        if structure_file.filename == '':
+            out += "No selected file\n"
+        if structure_file and allowed_file(structure_file.filename):
+            path = secure_filename(structure_file.filename)
+            structure_file.save(os.path.join(app.config['UPLOAD_FOLDER'], path))
+    if path is None:
+        path = request.form.get(f"last{filetype}", None)
+        if path is None:
+            out += "error as no file was uploaded and last one could not be reused\n"
+    return path
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -14,10 +44,26 @@ def io():
         args: list[str] = []
         if len(request.form) > 0:
             for key, value in request.form.items():
+                if not key.startswith("cli_"):
+                    continue
+                key.removeprefix("cli_")
                 args.append(f"--{key}")
                 if value != "":
                     args.append(str(value))
             inputdict = request.form
+
+            # read structure file
+            if structure_path := manage_uploaded_file(request, "structure"):
+                args.append("-fs")
+                args.append(structure_path)
+
+            # read elements file
+            if request.form.get("use_default_elements", "") != "on":
+                print("using custom elements file")
+                elements_path = manage_uploaded_file(request, "elements"):
+                args.append("-fe")
+                args.append(elements_path)
+
         else:
             args = request.args.get('cli-arguments', '').split(" ")
             inputdict = request.args
