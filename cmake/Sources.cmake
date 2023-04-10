67d5cac594239099ab2b1f25ca366c1944d41ebb
Jasmin B. Maglic <jmaglic@outlook.de>
2023-04-10 16:42:37 +0200

    Compartmentalise cmake sections for readability

---
+++ cmake/Sources.cmake
@@ -0,0 +1,24 @@
+
+# List of source files
+set(SOURCES
+  src/atomtree.cpp
+  src/base_guicontrol.cpp
+  src/base_cmdline.cpp
+  src/base_constr.cpp
+  src/base_event.cpp
+  src/base_guicontrol.cpp
+  src/base_init.cpp
+  src/cavity.cpp
+  src/controller.cpp
+  src/crystallographer.cpp
+  src/griddata.cpp
+  src/importmanager.cpp
+  src/misc.cpp
+  src/model.cpp
+  src/model_filereading.cpp
+  src/model_outputfiles.cpp
+  src/space.cpp
+  src/special_chars.cpp
+  src/vector.cpp
+  src/voxel.cpp
+)
