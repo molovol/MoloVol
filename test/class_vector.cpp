3b828037b230415f5482cf31b17fedb4545e8b03
Jasmin B. Maglic <jmaglic@outlook.de>
2023-04-15 17:04:02 +0200

    Add unit tests for vector access methods

--- test/class_vector.cpp
+++ test/class_vector.cpp
@@ -44,4 +44,33 @@ int main(){
 
   }
 
+  { // Access methods
+    Vector vec(1, 2, 3);
+    assert(vec[0] == 1);
+    assert(vec[1] == 2);
+    assert(vec[2] == 3);
+
+    vec.setCoordinate(0,4);
+    vec.setCoordinate(1,5);
+    vec.setCoordinate(2,6);
+
+    assert(vec.getCoordinate(0) == 4);
+    assert(vec.getCoordinate(1) == 5);
+    assert(vec.getCoordinate(2) == 6);
+
+    vec[0] = 7;
+    vec[1] = 8;
+    vec[2] = 9;
+
+    assert(vec.getCoordinate(0) == 7);
+    assert(vec.getCoordinate(1) == 8);
+    assert(vec.getCoordinate(2) == 9);
+
+    [[maybe_unused]] const Vector c_vec = vec;
+    
+    assert(c_vec.getCoordinate(0) == c_vec[0]);
+    assert(c_vec.getCoordinate(1) == c_vec[1]);
+    assert(c_vec.getCoordinate(2) == c_vec[2]);
+  }
+
 }
