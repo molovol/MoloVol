f4f8c08734f566870b64f1bf13f92ce79662677a
Jasmin B. Maglic <jmaglic@outlook.de>
2024-02-22 02:57:15 +0100

    Implement molecule rendering after calculation

--- src/render_frame.cpp
+++ src/render_frame.cpp
@@ -124,13 +124,23 @@ void RenderFrame::UpdateSurface(const Container3D<Voxel>& surf_data, const bool 
 }
 
 void RenderFrame::UpdateMolecule(const std::vector<Atom>& all_atoms) {
-  {
-    AtomTree atomtree(all_atoms);
+  
+  AtomTree atomtree(all_atoms);
 
-    for (size_t at_id = 0; at_id < all_atoms.size(); ++at_id) {
-      const Atom& at = all_atoms[at_id];
-      std::vector<size_t> closest = atomtree.listAllWithin(at.getPos(), 0);
-      std::cout << at.symbol << " " << closest.size() << std::endl;
+  molecule->Initialize();
+
+  std::vector<vtkAtom> atom_objs;
+  for (const Atom& at : all_atoms) {
+    atom_objs.push_back(molecule->AppendAtom(at.number, at.pos_x, at.pos_y, at.pos_z));
+  }
+
+  for (size_t at_id = 0; at_id < all_atoms.size(); ++at_id) {
+    const Atom& at = all_atoms[at_id];
+    std::vector<size_t> closest = atomtree.listAllWithin(at.getPos(), 0);
+    for (const size_t nb_id : closest) {
+      if (nb_id != at_id) {
+        molecule->AppendBond(atom_objs[at_id], atom_objs[nb_id], 1);
+      }
     }
   }
 }
@@ -290,33 +300,6 @@ void RenderFrame::InitRenderWindow() {
   surface->ComputeNormalsOn();
 
   molecule->Initialize();
-  /*vtkAtom O1 = molecule->AppendAtom(8, 3.0088731969, 1.1344098673, 0.9985902874);
-  vtkAtom O2 = molecule->AppendAtom(8, -0.2616286966, 2.7806709534, 0.7027800226);
-  vtkAtom C1 = molecule->AppendAtom(6, -2.0738607910, 1.2298524695, 0.3421802228);
-  vtkAtom C2 = molecule->AppendAtom(6, -1.4140240045, 0.1045928523, 0.0352265378);
-  vtkAtom C3 = molecule->AppendAtom(6, 0.0000000000, 0.0000000000, 0.0000000000);
-  vtkAtom C4 = molecule->AppendAtom(6, 1.2001889412, 0.0000000000, 0.0000000000);
-  vtkAtom C5 = molecule->AppendAtom(6, -1.4612030913, 2.5403617582, 0.6885503164);
-  vtkAtom C6 = molecule->AppendAtom(6, 2.6528126498, 0.1432895796, 0.0427014196);
-  vtkAtom H1 = molecule->AppendAtom(1, -3.1589178142, 1.2268537165, 0.3536340040);
-  vtkAtom H2 = molecule->AppendAtom(1, -1.9782163251, -0.7930325394, -0.1986937306);
-  vtkAtom H3 = molecule->AppendAtom(1, 3.0459155564, 0.4511167867, -0.9307386568);
-  vtkAtom H4 = molecule->AppendAtom(1, 3.1371551056, -0.7952192984, 0.3266426961);
-  vtkAtom H5 = molecule->AppendAtom(1, 2.3344947615, 1.8381683043, 0.9310726537);
-  vtkAtom H6 = molecule->AppendAtom(1, -2.1991803919, 3.3206134015, 0.9413825084);
-  molecule->AppendBond(C1, C5, 1);
-  molecule->AppendBond(C1, C2, 2);
-  molecule->AppendBond(C2, C3, 1);
-  molecule->AppendBond(C3, C4, 3);
-  molecule->AppendBond(C4, C6, 1);
-  molecule->AppendBond(C5, O2, 2);
-  molecule->AppendBond(C6, O1, 1);
-  molecule->AppendBond(C5, H6, 1);
-  molecule->AppendBond(C1, H1, 1);
-  molecule->AppendBond(C2, H2, 1);
-  molecule->AppendBond(C6, H3, 1);
-  molecule->AppendBond(C6, H4, 1);
-  molecule->AppendBond(O1, H5, 1);*/
   molmapper->SetInputData(molecule);
   molmapper->UseLiquoriceStickSettings();
   molactor->SetMapper(molmapper);
