e2ed3cf92e66187576b82869f37ffdbb42bb3753
Benedikt S. Vogler <mail@benediktsvogler.com>
2024-03-17 21:01:56 +0100

    version in variable

--- container/install_wxwidget.sh
+++ container/install_wxwidget.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-DEBIAN_FRONTEND="noninteractive" apt-get update & apt-get -y install tzdata build-essential manpages-dev libgtk2.0-dev wget
 #version in varialbe
-wget https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.5/wxWidgets-3.1.5.tar.bz2
-tar xvf wxWidgets-3.1.5.tar.bz2
-cd wxWidgets-3.1.5
+WX_VERSION=3.1.5
+DEBIAN_FRONTEND="noninteractive" apt-get update & apt-get -y install tzdata build-essential manpages-dev libgtk2.0-dev wget
+wget https://github.com/wxWidgets/wxWidgets/releases/download/v${WX_VERSION}/wxWidgets-${WX_VERSION}.tar.bz2
+tar xvf wxWidgets-${WX_VERSION}.tar.bz2
+cd wxWidgets-${WX_VERSION}
 ./configure --disable-shared --enable-unicode
 make install
