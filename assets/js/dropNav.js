32f32bf52177f392dd78cebbc14792bba9c4eae6
Jasmin B. Maglic <jmaglic@outlook.de>
2024-11-21 19:23:13 +0100

    Add javascript function to handle drop down

---
+++ assets/js/dropNav.js
@@ -0,0 +1,39 @@
+
+var dropped = false;
+
+function modifyClass(className, modiType, className2) {
+  const arr = document.getElementsByClassName(className);
+
+  switch (modiType) {
+    case "add":
+
+      for (i = 0; i < arr.length; i++) {
+        arr[i].classList.add(className2);
+      }
+        
+      break;
+    case "remove":
+
+      for (i = 0; i < arr.length; i++) {
+        arr[i].classList.remove(className2);
+      }
+
+      break;
+    default:
+      console.log("Function was called with unknown modification type."); 
+  }
+}
+
+function dropNav() {
+  // Update class lists to display navigation bar drop down menu on narrow screens
+
+  if (!dropped) {
+    modifyClass("navtab", "add", "responsive");
+  }
+  else {
+    modifyClass("navtab", "remove", "responsive");
+  }
+
+  dropped = !dropped;
+}
+
