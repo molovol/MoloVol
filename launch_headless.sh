d8c252b60bd37672b40f69d6b8b4d15053985b86
Benedikt S. Vogler <developer@benediktsvogler.com>
2025-01-10 14:34:45 +0100

    remove headless launcher

---
+++ launch_headless.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# This script launches the headless version of the application by creating fake display so that wxwidgets is happy.
+export DISPLAY=:1.0
+Xvfb :1 -screen 0 1024x768x16 &> xvfb.log  &
+molovol "$@"
